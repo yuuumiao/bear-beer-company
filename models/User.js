a5c3372f01890ea50ab47d83ed585043fdaa2b02
Xuan Tran <tran.ttxuan91@gmail.com>
2020-12-14 15:35:54 +0100

    merge

--- models/User.js
+++ models/User.js
@@ -5,12 +5,12 @@ const UserSchema = new Schema({
 
     firstName: String,
     lastName: String,
-    isAdult: Boolean, 
-    email: {type: String, unique: true} ,
+    isAdult: Boolean,
+    email: { type: String, unique: true },
     address: {
-            streetNumber: Number,
-            street: String,
-            city: String,
+        streetNumber: Number,
+        street: String,
+        city: String,
         postcode: String,
         telephone: String
     },
@@ -18,17 +18,9 @@ const UserSchema = new Schema({
     role: { type: String, enum: ["user", "admin"], default: user },
     hashPassword: { type: string, require: true }
 },
-<<<<<<< HEAD
     { timestamps: true })
-
-=======
-{ timestamps: true })
 //gopgle map api with the auto fill the address 
 //8 lines code, there is a tutorial
-<<<<<<< HEAD
->>>>>>> fb4c5536d797e4654f0b71b01c1340e004597f19
-const UserModel = mongoose.model("user", BeerSchema);
-=======
+
 const UserModel = mongoose.model("user", UserSchema);
->>>>>>> a6bd53edc1a5c7ca63b0834aa3fd8fd64e30e0f9
 module.exports = UserModel;
