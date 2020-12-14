de2a41398bc66b9786aaa0b587a70bff92af6c13
Menhaj safi <minhaj.sahil01@gmail.com>
2020-12-14 12:14:24 +0100

    new folder dashbourd on viewa

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,26 +1,38 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/dashbourd", (req, res, next) => {
+// GET to show all the products
+router.get("/", (req, res, next) => {
   res.render("products");
 });
 
+// GET to manage all the products
 router.get("/products-manage", (req, res, next) => {
-  res.render("products");
+  res.render("products-manage");
 });
 
+// GET to add new the products
 router.get("/product-add", (req, res, next) => {
-  res.render("products");
+  res.render("product-add");
 });
 
+// GET to post products
 router.post("/product-add", (req, res, next) => {
   res.redirect("/dashboard");
 });
 
+// GET to edit the products
 router.get("/product-edit/:id", (req, res, next) => {
-  res.render("products");
+  res.render("product-edit");
 });
+
+// GET to post the edit products
 router.post("/product-edit/:id", (req, res, next) => {
+  res.redirect("/dashboard");
+});
+
+// GET to delet the product
+router.get("/delete/:id", (req, res, next) => {
   res.redirect("/dashboard");
 });
 
