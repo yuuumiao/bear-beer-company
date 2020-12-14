b4429c551f4aebd1af3b1ecab9c961caf4707e2d
Menhaj safi <minhaj.sahil01@gmail.com>
2020-12-14 11:44:34 +0100

    dashboard filse

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,3 +1,27 @@
 var express = require("express");
 var router = express.Router();
+
+router.get("/dashbourd", (req, res, next) => {
+  res.render("products");
+});
+
+router.get("/products-manage", (req, res, next) => {
+  res.render("products");
+});
+
+router.get("/product-add", (req, res, next) => {
+  res.render("products");
+});
+
+router.post("/product-add", (req, res, next) => {
+  res.redirect("/dashboard");
+});
+
+router.get("/product-edit/:id", (req, res, next) => {
+  res.render("products");
+});
+router.post("/product-edit/:id", (req, res, next) => {
+  res.redirect("/dashboard");
+});
+
 module.exports = router;
