5f2f86d375fe7c460193ce68f2026502d24ab0d8
Menhaj safi <minhaj.sahil01@gmail.com>
2020-12-14 20:12:54 +0100

    dashboard add, edit, delete

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const BearModel = require("./../models/Product");
+const CartModel = require("./../models/Cart");
 
 // GET to show all the products
 router.get("/", (req, res, next) => {
@@ -17,28 +19,54 @@ router.get("/products-manage", async (req, res, next) => {
 });
 
 // GET to add new the products
-router.get("/product-add", (req, res, next) => {
-  res.render("dashboard/product-add");
+router.get("/product-add", async (req, res, next) => {
+  try {
+    const carts = await CartModel.find();
+    res.render("dashboard/product-add", { carts });
+  } catch (err) {
+    next(err)
+  }
 });
 
 // GET to post products
-router.post("/product-add", (req, res, next) => {
-  res.redirect("/dashboard");
+router.post("/product-add", async (req, res, next) => {
+  const newBear = { ...req.body };
+  try {
+    await BearModel.create(newBear);
+    res.redirect("/dashboard");
+  } catch (err) {
+    next(err)
+  }
+  
 });
 
 // GET to edit the products
-router.get("/product-edit/:id", (req, res, next) => {
-  res.render("dashboard/product-edit");
+router.get("/product-edit/:id", async (req, res, next) => {
+  const bear = await BearModel.findById(req.params.id);
+  const carts = await CartModel.find();
+  console.log(bear);
+  res.render("dashboard/product-edit", {bear, carts});
 });
 
 // GET to post the edit products
-router.post("/product-edit/:id", (req, res, next) => {
-  res.redirect("/dashboard");
+router.post("/product-edit/:id", async (req, res, next) => {
+  try {
+    const bear = req.body
+    await BearModel.findByIdAndUpdate(req.params.id, sneaker);
+    res.redirect("/dashboard")
+  } catch (err) {
+    next(err)
+  }
 });
 
 // GET to delete the product
 router.get("/delete/:id", (req, res, next) => {
-  res.redirect("/dashboard");
+  try {
+    await BearModel.findByIdAndRemove(req.params.id);
+    res.redirect("/dashboard");
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
